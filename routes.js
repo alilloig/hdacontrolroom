1b5cd3fb0a40e09f22e805a274aa9c042f94d999
Álvaro Lillo Igualada <alilloig@gmail.com>
2014-08-17 12:39:48 -0700

    Correccion

--- routes.js
+++ routes.js
@@ -2,17 +2,17 @@ var mochad = require('./resources/mochad.js');
 var devices = require('./resources/devices.js');
 
 app.io.route('device', {
-  turnOn: function(req) {
-  	console.log('Enviando el comando para encender el dispositivo '+code);
+	turnOn: function(req) {
+		console.log('Enviando el comando para encender el dispositivo '+code);
 		mochad.turnOn(req.data);
-  },
-  turnOff: function(req){
-  	console.log('Enviando el comando para apagar el dispositivo '+code);
+	},
+	turnOff: function(req){
+		console.log('Enviando el comando para apagar el dispositivo '+code);
 		mochad.turnOff(req.data);
-  },
-  checkStatus: function(req) {
-  	devices.checkStatus(req);
-  }
+	},
+	checkStatus: function(req) {
+		devices.checkStatus(req);
+	},
 	startListening: function(req){
 		mochad.readInput(req.io);//le pasamos la conexion con el websocket forzando a tope
 	}
