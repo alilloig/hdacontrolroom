846dd91f89a88fb08324ff1209f9f95856f771d4
Álvaro Lillo Igualada <alilloig@gmail.com>
2014-08-24 14:56:03 +0200

    Metemos las rutas en app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 express = require('express.io');
 app = express().http().io();
 
+var mochad = require('./resources/mochad.js');
+var devices = require('./resources/devices.js');
+
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
   app.use(express.logger('dev')); // log every request to the console
@@ -13,8 +16,18 @@ app.get('/', function (req,res){
 	res.sendfile(__dirname + '/index.html');
 });
 
-app.io.route('startSystem', function(req) {
-	console.log("Recibido evento iniciador del sistema");
+app.io.route('turnOn', function(req) {
+	console.log('Enviando el comando para encender el dispositivo '+req.data.code);
+	mochad.sendOn(req.data.code);
+});
+	
+app.io.route('turnOff', function(req) {
+	console.log('Enviando el comando para apagar el dispositivo '+req.data.code);
+	mochad.sendOff(req.data.code);
 });
 
-GLOBAL.app = app;
+app.io.route('startSystem', function(req) {
+	console.log("Recibido evento iniciador del sistema");
+	devices.checkStatus();
+	mochad.readInput();
+});
