4d101c6858739ac0b53b88c21121e73626154457
Álvaro Lillo Igualada <alilloig@gmail.com>
2014-08-24 13:55:49 +0200

    mira chechu....

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-app = require('express.io')();
-app.http().io();
+express = require('express.io');
+app = express().http().io();
 
 app.configure(function() {
-  app.use(app.static(__dirname + '/public'));
-  app.use(app.logger('dev')); // log every request to the console
-  app.use(app.bodyParser()); //pull into POST
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.logger('dev')); // log every request to the console
+  //app.use(express.bodyParser()); //pull into POST
 });
 
 app.listen(80);// startup our app at http://localhost:80
