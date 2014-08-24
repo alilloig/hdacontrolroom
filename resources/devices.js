d3f2806696aa7f420990add32ce9f20317dbd820
Álvaro Lillo Igualada <alilloig@gmail.com>
2014-08-24 22:22:18 +0200

    Añadido log a devices

--- resources/devices.js
+++ resources/devices.js
@@ -1,8 +1,9 @@
-var list = [{label:'lamparaRoja',code:'a1',status:false},
+var list = [{label:'lamparaRoja',code:'a9',status:false},
 						{label:'persianaSalon',code:'a2',status:false},
 						{label:'lamparaPie',code:'a3',status:false}];
 
 exports.turnItOn = function (code){
+	console.log("Encendiendo dispositivo: "+code);
 	for (var i=0;i<list.length;i++){
 		if(list[i].code == code){
 			app.io.broadcast('turnOn',{code:list[i].code});
@@ -13,6 +14,7 @@ exports.turnItOn = function (code){
 };
 
 exports.turnItOff = function (code){
+	console.log("Apagando dispositivo: "+code);
 	for (var i=0;i<list.length;i++){
 		if(list[i].code == code){
 			app.io.broadcast('turnOff',{code:list[i].code});
